cb39154e6be56d2d72f15ca95612a7df436576a2
Harald Wilhelmi <harald.wilhelmi@uni-heidelberg.de>
2025-03-26 17:14:20 +0100

    Styling the footer

--- www/js/common.v2.js
+++ www/js/common.v2.js
@@ -1495,19 +1495,15 @@ const apiCallsMixin = {
 const footer = document.querySelectorAll('footer').item(0)
 footer.innerHTML = `
     <!-- Add custom footer for DHART -->
-    <div>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-6">
-              <a href="https://www.klinikum.uni-heidelberg.de/">
-                <img src="img/by_domain/dhart/171029Logo_UKHD_dt_positiv_RGB.png" style="max-height:100px;" title="Heidelberg University Hospital"/>
-              </a>
-            </div>
-            <div class="col-sm-6">
-              <a href="https://dzhk.de/">
-                <img src="img/by_domain/dhart/dzhk.png" style="max-height:100px;" title="German Center for Cardiovascular Research"/>
-              </a>
-            </div>
-          </div>
-        </div>
+    <div style="display: flex">
+      <div style="flex-grow: 1">
+      <a href="https://www.klinikum.uni-heidelberg.de/">
+        <img src="img/by_domain/dhart/171029Logo_UKHD_dt_positiv_RGB.png" style="max-height:100px;" title="Heidelberg University Hospital"/>
+      </a>
+      </div>
+      <div style="flex-grow: 1">
+        <a href="https://dzhk.de/">
+          <img src="img/by_domain/dhart/dzhk.png" style="max-height:100px;" title="German Center for Cardiovascular Research"/>
+        </a>
+      </div>
     </div>`;
