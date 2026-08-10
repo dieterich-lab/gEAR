cb5b817dafc7f3a012342bf0ff8665b1831236b1
Harald Wilhelmi <harald.wilhelmi@uni-heidelberg.de>
2026-08-10 16:10:48 +0200

    Trying to fix sc_workbench.js (3 redone)

--- www/js/sc_workbench.js
+++ www/js/sc_workbench.js
@@ -954,6 +954,7 @@ document.querySelector(UI.analysisSelect).addEventListener("change", async (even
         // Jump to the primary filter step
         document.querySelector(UI.primaryFilterSection).click();
         document.querySelector(`a[href='${UI.primaryFilterSection}']`).click();
+        await applyPrimaryFilter();
         return;
     }
     createToast("Loading stored analysis", "is-info");
